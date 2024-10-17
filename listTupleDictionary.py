aafb4ed77219975af6a021bbcf9878125e3c4fc5
Programmer <baxacap@gmail.com>
2024-10-17 19:34:22 +0500

    Create listTupleDictionary.py

---
+++ listTupleDictionary.py
@@ -0,0 +1,23 @@
+#1   
+a=[1,2,3,4,5,6,7,8,9,10]
+print(a)
+
+#2
+a.append(11)
+a.append(12)
+print(a)
+
+#3
+a.insert(5, 111)
+print(a)
+
+#4
+b=["Olma", "Olcha", "Nok", "Uzum", "Gilos"]
+b.pop(-1)
+print(b)
+b.remove("Olma")
+print(b)
+
+#5
+c={"Gentra":"Qora", "Nexia 2":"Oq", "Matiz":"Ko'k"}
+print(c)
